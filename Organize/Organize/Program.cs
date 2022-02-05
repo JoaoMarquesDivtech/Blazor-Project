c14dbbbf6057a118583134102c3b2fe77bf7bccb
jopdaltro <jopdaltro@gmail.com>
2022-02-05 17:00:54 -0300

    Organização do projeto

--- Organize/Organize/Program.cs
+++ Organize/Organize/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Organize
+namespace BlazorProject
 {
     public class Program
     {
--- Organize/Organize/Startup.cs
+++ Organize/Organize/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Organize
+namespace BlazorProject
 {
     public class Startup
     {
